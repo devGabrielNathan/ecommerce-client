e1ffa041daf59e00f2d3377545604ab665fb821f
Gabriel Nathan <devgabrielnathan@gmail.com>
2025-08-21 09:44:32 -0300

    removing Icategory
    The API return is of type ICategory, but it is just a reference to the primary key, so it only returns the id, which is a string and not an object.

--- src/interfaces/subcategory.ts
+++ src/interfaces/subcategory.ts
@@ -1,9 +1,8 @@
-import { ICategory } from './category';
 import { Status } from '../enums/status';
 
 export interface ISubCategory {
     id: string;
     name: string;
     status: Status;
-    category: ICategory;
+    category: string;
 }
